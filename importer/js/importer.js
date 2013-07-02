fee5bcb39564cdd26ce9eac516c9aef703dfb6a8
Francisco Jordano <arcturus@ardeenelinfierno.com>
2013-07-02 13:37:25 +0100

    Redirect oauth requests based on gecko support

--- importer/js/importer.js
+++ importer/js/importer.js
@@ -1,4 +1,4 @@
-var googleAuth = 'https://accounts.google.com/o/oauth2/auth?scope=https://www.google.com/m8/feeds/&response_type=token&redirect_uri=http://redirector.cloudfoundry.com/&approval_prompt=force&client_id=206115911344.apps.googleusercontent.com';
+var googleAuth = 'https://accounts.google.com/o/oauth2/auth?scope=https://www.google.com/m8/feeds/&response_type=token&redirect_uri=http://redirector.cloudfoundry.com/redirect.html&approval_prompt=force&client_id=206115911344.apps.googleusercontent.com';
 var authWindow;
 
 var menu = document.getElementById('importer-menu');
